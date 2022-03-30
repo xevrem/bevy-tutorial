afc8cf99351b4eed44473c6fae451318f3b9518d
erika jonell <erika.jonell@gmail.com>
2022-03-30 06:53:40 -0400

    current progress

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+mod ascii;
+mod debug;
+mod player;
+mod tilemap;
+
+use ascii::AsciiPlugin;
+use debug::DebugPlugin;
+use player::PlayerPlugin;
+use tilemap::TileMapPlugin;
+
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+pub const TILE_SIZE: f32 = 0.1;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
+pub enum GameState {
+    Overworld,
+    Combat,
+}
+
+fn main() {
+    let height = 900.0;
+
+    App::new()
+        .add_state(GameState::Overworld)
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: height * RESOLUTION,
+            height,
+            title: "Bevy Tutorial".to_string(),
+            vsync: true,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(AsciiPlugin)
+        .add_plugin(TileMapPlugin)
+        .add_plugin(DebugPlugin)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
+    camera.orthographic_projection.left = 1.0 * RESOLUTION;
+    camera.orthographic_projection.right = -1.0 * RESOLUTION;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
